ed35991ee5d40c9b202c65ce0de576cd3e0a2f38
devlewis <devree.lewis@gmail.com>
2021-03-25 16:08:16 -0400

    add waterfall, disabling functionality

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let play = true;
 let rainDisabled = false;
+let waterfallDisabled = false;
 let particlesArray = [];
 let numberOfParticles;
 let animationId;
@@ -13,6 +14,7 @@ let directionX;
 let directionY;
 let color;
 let pattern;
+let trail;
 
 class Particle {
   constructor(x, y) {
@@ -27,11 +29,13 @@ class Particle {
   update() {
     if (this.y > canvas.height) {
       this.y = 0 - this.size;
-      this.weight = weight;
+      this.weight = 2;
       this.x = Math.random() * canvas.width * 2;
     }
-    if (this.y) this.weight += Math.random() * 25 + 10;
-    this.y -= this.directionY;
+    this.weight += 0.05;
+    pattern === "waterfall"
+      ? (this.y += this.weight)
+      : (this.y -= this.directionY);
     this.x += this.directionX;
   }
 
@@ -39,14 +43,13 @@ class Particle {
     ctx.fillStyle = color;
     ctx.beginPath();
     switch (pattern) {
-      case "waterfall":
-        ctx.arc(this.x, this.y, this.size, 0, shape);
+      case "3rd pattern":
+        //new kind of drawing here
         break;
 
       default:
         ctx.arc(this.x, this.y, this.size, 0, shape);
     }
-    //ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2); //change last two digits for different shape of particel
     ctx.closePath();
     ctx.fill();
   }
@@ -63,7 +66,7 @@ function init() {
 init();
 
 function animate() {
-  ctx.fillStyle = `rgba(255, 255, 255, 0.25)`;
+  ctx.fillStyle = `rgba(255, 255, 255, ${trail})`;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
@@ -76,23 +79,48 @@ function animate() {
 animate();
 
 function playPause(animationId) {
-  if (rainDisabled) {
+  if (rainDisabled || waterfallDisabled) {
     play ? window.cancelAnimationFrame(animationId) : animate();
     play = !play;
   }
 }
 
 function rain() {
+  ctx.fillStyle = `rgba(255, 255, 255)`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   document.getElementById("rain").disabled = !rainDisabled;
   rainDisabled = !rainDisabled;
+  waterfallDisabled && (waterfallDisabled = false);
+  document.getElementById("waterfall").disabled = waterfallDisabled;
   particlesArray = [];
   color = "blue";
   numberOfParticles = 1000;
   size = 5;
   directionX = -1;
   directionY = -2;
-  weight = 0.5;
+  weight = Math.random() * 1 + 1;
   shape = 2;
   pattern = "rain";
+  trail = 0.25;
+  init();
+}
+
+function waterfall() {
+  ctx.fillStyle = `rgba(255, 255, 255)`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  document.getElementById("waterfall").disabled = !waterfallDisabled;
+  waterfallDisabled = !waterfallDisabled;
+  rainDisabled && (rainDisabled = false);
+  document.getElementById("rain").disabled = rainDisabled;
+  particlesArray = [];
+  color = "lightblue";
+  numberOfParticles = 100;
+  size = 10;
+  directionX = 1;
+  directionY = 0;
+  weight = 2;
+  shape = Math.PI * 2;
+  pattern = "waterfall";
+  trail = 0.01;
   init();
 }
